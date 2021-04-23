661ace9a23fb97db59af6b5a18e26acb63344cef
lalugue <51893731+lalugue@users.noreply.github.com>
2021-04-23 19:09:12 +0800

    add title div, run prettier

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,57 +1,65 @@
 <template>
-  <div>
-     <DxScheduler
-      id="scheduler"
-      :data-source="dataSource"
-      :views="views"
-      currentView="day"
-      :currentDate="currentDate"
-      :firstDayOfWeek="0"
-      :startDayHour="9"
-      :endDayHour="23"
-      :showAllDayPanel="false"
-      :height="600">
-     </DxScheduler>
+  <div class="long-title">
+    <h3>DXCinema Upcoming Movies</h3>
   </div>
+  
+  <DxScheduler
+    id="scheduler"
+    :data-source="dataSource"
+    :views="views"
+    currentView="day"
+    :currentDate="currentDate"
+    :firstDayOfWeek="0"
+    :startDayHour="9"
+    :endDayHour="23"
+    :showAllDayPanel="false"
+    :height="600"
+  >
+  </DxScheduler>
 </template>
 
 <script>
+import DxScheduler from "devextreme-vue/scheduler";
 
-import DxScheduler from 'devextreme-vue/scheduler';
-
-import { data, rows, seats } from './data.js';
+import { data, rows, seats } from "./data.js";
 
 export default {
-  name: 'Home',
+  name: "Home",
   props: {
-    msg: String
+    msg: String,
   },
   components: {
-    DxScheduler
+    DxScheduler,
   },
-  data(){
-     return {
-        dataSource: data,
-        rows: rows,
-        seats: seats,
-        views: ['day', 'timelineDay'],
-        currentDate: new Date(2015, 4, 25)
-     }
+  data() {
+    return {
+      dataSource: data,
+      rows: rows,
+      seats: seats,
+      views: ["day", "timelineDay"],
+      currentDate: new Date(2015, 4, 25),
+    };
   },
   computed: {
-    getText(){
-       return `Click count: ${this.count}`;
-    }
+    getText() {
+      return `Click count: ${this.count}`;
+    },
   },
   methods: {
-    clickHandler(){
-        this.count+=1;
-    }
-  }
-}
+    clickHandler() {
+      this.count += 1;
+    },
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.long-title h3 {
+  font-family: "Segoe UI Light", "Helvetica Neue Light", "Segoe UI",
+    "Helvetica Neue", "Trebuchet MS", Verdana;
+  font-weight: 200;
+  font-size: 28px;
+  text-align: center;
+  margin-bottom: 20px;
+}
 </style>
