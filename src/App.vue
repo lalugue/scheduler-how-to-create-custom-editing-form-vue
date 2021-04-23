27e7f6d5ba9a89c5b1a9ffd85a921752d31412e8
lalugue <51893731+lalugue@users.noreply.github.com>
2021-04-23 19:04:13 +0800

    remove template style

--- src/App.vue
+++ src/App.vue
@@ -17,12 +17,4 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  margin: 50px 50px;
-  width: 90vh;
-}
 </style>
