e437c4a3670e4be34ebe15938f33dc79fcf972dd
lalugue <51893731+lalugue@users.noreply.github.com>
2021-04-23 19:44:49 +0800

    port template

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -30,7 +30,48 @@
         height="100%"
       >       
       <img :src="editAppointmentData.image" class="dx-field-label" />
-      
+      <div class="dx-field-label">
+        <p>
+          <b>{{ editAppointmentData.text }}</b>
+        </p>
+        <p>Year: {{ editAppointmentData.year }}</p>
+        <p>Duration: {{ editAppointmentData.duration }} minutes</p>
+      </div>
+
+      <div class="dx-field-label">
+        <b>{{ formatDate(editAppointmentData.startDate, "shortTime") }} -
+          {{ formatDate(editAppointmentData.endDate, "shortTime") }}</b>
+      </div>
+
+      <div class="dx-field-label"><b>Price ($): </b></div>
+      <div id="priceView" class="dx-field-label">
+        {{
+          editAppointmentData.seatRow && editAppointmentData.seatNumber
+            ? setSeatPrice(
+                editAppointmentData.price,
+                editAppointmentData.seatRow
+              )
+            : "Pick a seat for pricing"
+        }}
+      </div>      
+
+      <DxSelectBox
+        id="rowView"
+        class="dx-field-label"
+        :dataSource="rows"
+        :width="400"
+        placeholder="Pick a row"
+        v-model:value="editAppointmentData.seatRow">
+      </DxSelectBox>
+
+      <DxSelectBox
+        id="seatView"
+        class="dx-field-label"
+        :dataSource="seats"
+        :width="400"
+        placeholder="Pick a seat"
+        v-model:value="editAppointmentData.seatNumber">
+      </DxSelectBox>
       </DxScrollView>
     </template>
   </DxPopup>
@@ -41,7 +82,8 @@
 import { DxScheduler } from "devextreme-vue/scheduler";
 import { DxPopup } from "devextreme-vue/popup";
 import { DxScrollView } from "devextreme-vue/scroll-view"
-//import {DxSelectBox} from "devextreme-vue/select-box"
+import {DxSelectBox} from "devextreme-vue/select-box"
+import { formatDate } from "devextreme/localization";
 
 import { data, rows, seats } from "./data.js";
 
@@ -51,7 +93,7 @@ export default {
     DxScheduler,
     DxPopup,
     DxScrollView,
-    //DxSelectBox
+    DxSelectBox
   },
   data() {
     return {
@@ -73,6 +115,7 @@ export default {
         this.isCustomPopupVisible = true;
       }      
     },
+    formatDate
   },
 };
 </script>
